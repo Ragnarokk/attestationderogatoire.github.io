b26a119ee610620752f8543fc886809e968c42a2
Jean-Maurice Martin <jmmartin.martin1@gmail.com>
2020-04-29 08:47:35 +0200

    Downloads examples
    - When submitted, the form now downloads an example
    of the pdf attestation with a stub qrcode

--- index.js
+++ index.js
@@ -59,6 +59,40 @@ var debug = new Vue({
                     this.errors = true;
                 }
             }
+
+            if ( this.errors === false ) {
+                const img = new Image();
+                img.onload = function () {
+                    // we create a temporary canvas to draw the informations on the form
+                    const canvas = document.createElement('canvas');
+                    canvas.width = this.width;
+                    canvas.height = this.height;
+                    const ctx = canvas.getContext('2d');
+
+                    const qrimage = new VanillaQR({
+                        url: 'lolilol',
+                        width: 400,
+                        height: 400,
+                        colorLight: "#FFFFFF",
+                        colorDark: "#000000",
+                        ecclevel: 1,
+                        noBorder: true,
+                    }).toImage("png");
+
+                    ctx.drawImage(img, 0, 0);
+
+                    qrimage.onload = function () {
+                        ctx.drawImage(qrimage, 100, 100);
+
+                        const pdf = new jsPDF();
+                        const imgData = canvas.toDataURL("image/jpeg", 1.0);
+
+                        pdf.addImage(imgData, 'JPEG', 0, 0);
+                        pdf.save('attestation.pdf');
+                    }
+                }
+                img.src = 'attestation.jpg';
+            }
         }
     },
     components: {
