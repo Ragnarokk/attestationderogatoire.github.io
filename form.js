38a0bd4e645fc9be6c4f1335a3dd9d51db955035
Jean-Maurice Martin <jmmartin.martin1@gmail.com>
2020-04-29 14:05:25 +0200

    Pdf placement
    - Adds all the simple fields on the PDF
    - Qrcodes are adjusted correctly

--- form.js
+++ form.js
@@ -1,37 +1,55 @@
 function completeAndDownloadForm( data ) {
     console.log(data);
+    let qrstring = 'Cree le: ' + data.creationdate + ' a ' + data.creationhour + '; ' +
+                     'Nom: ' + data.lastname + '; ' +
+                     'Prenom: ' + data.name + '; ' +
+                     'Naissance: ' + data.birthdate + ' a ' + data.birthplace + '; ' +
+                     'Adresse: ' + data.address + ' ' + data.city + '; ' +
+                     'Sortie: ' + data.creationdate + ' a ' + data.hikinghour + '; ' +
+                     'Motifs: ';
+    for ( motive of data.checkset ) {
+        qrstring += motive + '-';
+    }
+    if (qrstring.slice(-1) === '-') {
+        qrstring = qrstring.slice(0, -1);
+    }
     const img = new Image();
     img.onload = function () {
         // we create a temporary canvas to draw the informations on the form
         const canvas = document.createElement('canvas');
         canvas.width = this.width;
-        canvas.height = this.height;
+        canvas.height = this.height * 2;
         const ctx = canvas.getContext('2d');
 
         const qrimage = new VanillaQR({
-            url: 'lolilol',
-            width: 400,
-            height: 400,
+            url: qrstring,
+            width: 480,
+            height: 480,
             colorLight: "#FFFFFF",
             colorDark: "#000000",
-            ecclevel: 1,
+            ecclevel: 2,
             noBorder: true,
         }).toImage("png");
 
         ctx.drawImage(img, 0, 0);
-        ctx.font = "bold 15px sans-serif";
-        ctx.fillText(data.name + ' ' + data.lastname, 175, 218);
-        ctx.fillText(data.birthdate, 175, 252);
-        ctx.fillText(data.birthplace, 128, 285);
-        ctx.fillText(data.address, 189, 320);
+        ctx.font = "17px ArialMT";
+        ctx.fillText(data.name + ' ' + data.lastname, 175, 217);
+        ctx.fillText(data.birthdate, 175, 251);
+        ctx.fillText(data.birthplace, 128, 284);
+        ctx.fillText(data.address, 189, 319);
+        ctx.fillText(data.city, 152, 857);
+        ctx.fillText(data.creationdate, 130, 891);
+        ctx.fillText(data.hikinghour, 282, 891);
 
         qrimage.onload = function () {
-            ctx.drawImage(qrimage, 400, 400);
+            ctx.drawImage(qrimage, 591, 817, 135, 135);
 
             const pdf = new jsPDF();
             const imgData = canvas.toDataURL("image/jpeg", 1.0);
 
             pdf.addImage(imgData, 'JPEG', 0, 0);
+            pdf.addPage();
+            pdf.addImage(qrimage, 'PNG', 20, 20, 100, 100);
             pdf.save('attestation.pdf');
         }
     }
